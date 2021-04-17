70acfc31e17065afa744c5f416ed5792878d7e3a
Chris <code@draz.io>
2021-04-17 00:49:51 -0500

    setting up config boilerplate

--- spotify_visualizer.py
+++ spotify_visualizer.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 
+import argparse
 import spotipy
 
 from spotipy.oauth2 import SpotifyClientCredentials
 
+def loadConfig(aConfigFile):
+    with open(aConfigFile, 'r') as read_from:
+        for line in read_from:
+            key, value = line.split(maxsplit=1)
+            os.environ[key] = value.rstrip()
+
+def addArguments(aArgumentParser):
+    aArgumentParser.add_argument('-c', '--config', nargs='?', default='config.cfg', help='Specifies a new config file')
+
 if __name__ == '__main__':
+    argument_parser = argparse.ArgumentParser()
+    addArguments(argument_parser)
+    arguments = argument_parser.parse_args()
+    loadConfig(arguments.config)
+
     spotify_client = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
